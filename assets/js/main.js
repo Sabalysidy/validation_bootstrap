f7063d2a13caa57ad10a802a3d875ec3bedc3078
sidy <Sabalysidy1303@gmail.com>
2023-05-14 20:27:56 +0000

    ajout des fichiers

---
+++ assets/js/main.js
@@ -0,0 +1,68 @@
+document.addEventListener('DOMContentLoaded', () => {
+  "use strict";
+
+  //  Scroll top button
+
+  const scrollTop = document.querySelector('.scroll-top');
+  if (scrollTop) {
+    const togglescrollTop = function () {
+      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+    }
+    window.addEventListener('load', togglescrollTop);
+    document.addEventListener('scroll', togglescrollTop);
+    scrollTop.addEventListener('click', window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    }));
+  }
+
+  // Icons nav menu
+
+const mobileNavShow = document.querySelector('.mobile-nav-show');
+const mobileNavHide = document.querySelector('.mobile-nav-hide');
+
+document.querySelectorAll('.icon-nav-menu').forEach(el => {
+  el.addEventListener('click', function(event) {
+    event.preventDefault();
+    mobileNavToogle();
+  })
+});
+
+function mobileNavToogle() {
+  document.querySelector('body').classList.toggle('mobile-nav-active');
+  mobileNavShow.classList.toggle('d-none');
+  mobileNavHide.classList.toggle('d-none');
+}
+
+new Swiper('.slide', {
+  speed: 700,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  slidesPerView: 'auto',
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+
+    1200: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    }
+  }
+  
+});
+
+});
